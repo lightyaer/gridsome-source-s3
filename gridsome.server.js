37ffefb15094a73a8651802f9c4958eb0715412b
lightyaer <mmnyed@gmail.com>
2021-01-19 21:45:20 +0530

    TEMP: Transient Changes

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,43 +1,134 @@
 const _ = require("lodash");
-const {} = require("@gridsome/source-filesystem");
-const listAllS3Objects = require("./src/aws/listAllS3Objects");
-const initAWS = require("./src/aws/init");
+const AWS = require("aws-sdk");
+const chalk = require("chalk");
 
 module.exports = function (api, options) {
-  api.loadSource(({ addCollection }) => {
-    try {
-      const { aws, buckets, expiration, mode = "list", key = "Data" } = options;
+  api.loadSource(async (actions) => {
+    const { aws, buckets, mode = "list" } = options;
+    const s3Buckets = _.castArray(buckets);
+    AWS.config.update(aws);
+    const S3 = new AWS.S3();
 
-      const AWS = initAWS(aws);
+    const getS3List = async ({ Bucket, ContinuationToken }) => {
+      return await S3.listObjectsV2({
+        Bucket,
+        ContinuationToken,
+      })
+        .promise()
+        .catch((e) =>
+          console.log(`Error in listing objects from ${Bucket}, ${e}`)
+        );
+    };
 
-      _.each(buckets, async (bucket) => {
-        const allS3Objects = await listAllS3Objects(AWS.S3, {
+    const listAllObjects = async ({ bucket, mode = "list" }) => {
+      const allS3Objects = [];
+      const currentData = await getS3List({ Bucket: bucket });
+
+      const contents = _.get(currentData, "Contents", []);
+
+      const addObject = async (contents) => {
+        if (!_.isEmpty(contents)) {
+          for (let object of contents) {
+            if (mode === "list") {
+              allS3Objects.push({ ...object });
+            } else {
+              const fullObject = await getObject({
+                Bucket: bucket,
+                Key: object.Key,
+              });
+
+              allS3Objects.push({
+                ...JSON.parse(fullObject.Body.toString()),
+              });
+            }
+          }
+        } else {
+          console.error(
+            `Error processing objects from bucket "${bucket}". Is it empty?`
+          );
+        }
+      };
+
+      await addObject(contents);
+
+      const isTruncated = _.get(currentData, "IsTruncated", false);
+      const nextContinuationToken = _.get(
+        currentData,
+        "NextContinuationToken",
+        null
+      );
+
+      let nextToken = currentData && isTruncated && nextContinuationToken;
+
+      while (nextToken) {
+        const nextData = await getS3List({
           Bucket: bucket,
-          mode,
-          key,
+          ContinuationToken: nextToken,
+        });
+        const contents = _.get(nextData, "Contents", []);
+
+        await addObject(contents);
+
+        const isTruncated = _.get(nextData, "IsTruncated", false);
+        const nextContinuationToken = _.get(
+          nextData,
+          "NextContinuationToken",
+          null
+        );
+
+        nextToken = nextData && isTruncated && nextContinuationToken;
+      }
+
+      return allS3Objects;
+    };
+
+    const getObject = async ({ Bucket, Key }) => {
+      return await S3.getObject({
+        Bucket,
+        Key,
+      })
+        .promise()
+        .catch((e) =>
+          console.log(
+            `Error in getting object from ${Bucket} bucket and ${Key} key, ${e}`
+          )
+        );
+    };
+
+    try {
+      _.each(s3Buckets, async (bucket) => {
+        const collectionName = _.upperFirst(_.camelCase(bucket));
+
+        console.log(
+          chalk.green(
+            chalk.blue.underline.bold("Adding to collection") + collectionName
+          )
+        );
+
+        const collection = actions.addCollection({
+          typeName: collectionName,
         });
 
-        const bucketCollection = addCollection({
-          typeName: bucket,
-        });
+        let objects = await Promise.all(
+          await listAllObjects({
+            bucket,
+            mode,
+          })
+        );
+        console.log(objects);
 
-        _.each(allS3Objects, (object, index) => {
-          bucketCollection.addNode({
-            id: index,
-            ...object,
-          });
+        objects = _.flatten(objects);
+
+        objects.forEach((object, index) => {
+          collection.addNode({ id: index, ...object });
         });
       });
     } catch (e) {
       console.error(e);
     }
   });
-
-  // api.onCreateNode(({ internal: { typeName }, published }, options) => {
-  //   const { aws, buckets, expiration, mode = "list", key = "Data" } = options;
-  //
-  // });
 };
+
 module.exports.defaultOptions = () => ({
   aws: {
     accessKeyId: "",
@@ -46,6 +137,6 @@ module.exports.defaultOptions = () => ({
   },
   buckets: [],
   expiration: 0,
-  mode: "list",
-  keys: [],
+  mode: "object",
+  keys: "data",
 });
--- src/aws/init.js
+++
@@ -1,6 +0,0 @@
-const AWS = require("aws-sdk");
-
-export default (aws) => {
-  AWS.config.update(aws);
-  return AWS;
-};
